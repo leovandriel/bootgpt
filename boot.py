88f5f75aae5ed3ec28a05267f6199ffba9130c07
Leo Vandriel <git@leovandriel.com>
2023-10-27 12:19:49 -0700

    add run multiple prompts

--- boot.py
+++ boot.py
@@ -1,12 +1,12 @@
 import openai
 
-open("run_0.py", "w").write(
+exec(
     openai.ChatCompletion.create(
         api_key=open("key.txt").read(),
         model="gpt-3.5-turbo",
         temperature=0.001,
         messages=[
-            {"role": "user", "content": open("prompt_0.txt").read()},
+            {"role": "user", "content": open("prompt_boot.txt").read()},
         ],
     )
     .choices[0]
